1ff98565089ce173439134b8a3edfa8700cd8d0b
Alexander igho <alexbriwen@gmail.com>
2022-09-22 16:09:32 -0700

    Task 5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    from sys import argv
+    varLen = len(argv) - 1
 
-if __name__ == '__main__':
-    args = sys.argv
-    args.pop(0)
-    print(sum(map(int, args)))
+    result = 0
+    for i in range(1, varLen + 1):
+        result += int(argv[i])
+    print(result)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4 as h
+if __name__ == "__main__":
+    import hidden_4
 
-if __name__ == '__main__':
-    names = filter(lambda n: not n.startswith('__'), dir(h))
-    for name in names:
-        print(name)
+    name = dir(hidden_4)
+    for names in name:
+        if names[:2] != "__":
+            print(names)
